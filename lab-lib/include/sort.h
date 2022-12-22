acec1d72ca6b04640c2db20ce9f4b532d7dc3983
stas <stas@git.com>
2022-12-22 10:50:42 +0300

    change README.md file

--- lab-lib/include/sort.h
+++ lab-lib/include/sort.h
@@ -1,7 +1,15 @@
 
 #ifndef SORT_H
 #define SORT_H
+typedef struct {
+    int len;
+    char *string;
+
+    int cnt;
+} str;
+
 void select_sort(int *arr, int len);
+
 void swap_2(int *xp, int *yp);
 
 void bubble_sort(int *arr, int n);
@@ -13,5 +21,17 @@ void swap_1(int **arr, int n, int m1, int m2);
 int findmax(int **arr, int n, int m);
 
 void printout(int **arr, int n, int m);
+
+int str_len(const char *str);
+
+int count_odd_words(const char *str, int len);
+
+void swap_3(str *a, str *b);
+
+int partition(str *array, int low, int high);
+
+void quickSort(str *array, int low, int high);
+
+int main(int argc, char *argv[]);
 
 #endif
